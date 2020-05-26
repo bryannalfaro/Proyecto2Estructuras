ef41c0c46d8f68e9230d1a06879c19bfb691c855
Bryann Alfaro <alf19372@uvg.edu.gt>
2020-05-26 16:35:04 -0600

    correccion programa

--- proyecto.py
+++ proyecto.py
@@ -7,18 +7,14 @@ graphdb = GraphDatabase.driver(uri="neo4j://localhost:7687", auth= ("neo4j", "12
 
 session = graphdb.session()
 
+menu_filtros = "Desea utilizar otro filtro?\n1. Si\n2. No"
+filtros_disponibles = ["Recomendar por rango de costos", "Recomendar por servicio a domicilio", "Recomendar por especialidad", "Recomendar por ubicación",
+                       "Recomendar por valoracion"]
+numeracion = 0
 
 #WITH lugar.valoracion as rating, lugar.name as l
 #RETURN l, rating
 #ORDER BY rating DESC
-
-def sendQuery(query):
-    query +=' WITH lugar.valoracion as rating, lugar.name as l RETURN l, rating ORDER BY rating DESC'
-    print(query)
-    nodes = session.run(q1)
-    for node in nodes:
-        print(node[0])    
-
 
 opcion=0
 print("-----------------------------------------")
@@ -46,26 +42,71 @@ while(opcion != 6):
         print("~~~~~~~~~~~~~~~~~~~~~~~~~~~")
         
         precio = int(input())
-
-
-        if(precio==1):
-            q1 = "MATCH(precio {name: '10-150'}) <-- (lugar) RETURN lugar.name"
-            print("Los que estan en tu rango de precio son: ")
-            sendQuery(q1)
+        print(menu_filtros)
+        opcion_filtro = int(input())
+        
+        if(opcion_filtro == 1):
+            filtros_disponibles.remove("Recomendar por rango de costos")
+            for item in filtros_disponibles:
+                numeracion = numeracion + 1
+                print(str(numeracion) + ". " + item)
+            opcion_filtrado = int(input())
             
-        q1 = "MATCH(especialidad {name: 'Comida Rapida'}) <-- (lugar) RETURN lugar.name"
+        else:
+        
+        
+            if(precio==1):
+                q1 = "MATCH(precio {name: '10-150'}) <-- (lugar) RETURN lugar.name"
+                nodes = session.run(q1)
+                print("Los que estan en tu rango de precio son: ")
+                for node in nodes:
+                    print(node[0])
+            if(precio==2):
+                q1 = "MATCH(precio {name: '150-300'}) <-- (lugar) RETURN lugar.name"
+                nodes = session.run(q1)
+                print("Los que estan en tu rango de precio son: ")
+                for node in nodes:
+                    print(node[0])
+            if(precio==3):
+                q1 = "MATCH(precio {name: '300-500'}) <-- (lugar) RETURN lugar.name"
+                nodes = session.run(q1)
+                print("Los que estan en tu rango de precio son: ")
+                for node in nodes:
+                    print(node[0])
+            
+        
+    elif(opcion==2):
+        print("Estos tienen servicio")
+        
+        q1 = "MATCH(servicio {name: 'Si'}) <-- (lugar) RETURN lugar.name"
         nodes = session.run(q1)
-        print("Te recomendamos comer en: ")
+        print("Los que tienen servicio son: ")
         for node in nodes:
             print(node[0])
-            
+        
+    elif(opcion==3):
+        print("~~~~~~~~~~~~~~~~~~~~~~~~~~")
+        print("Ingrese su especialidad: ")
+        print("1. Comida rápida")
+        print("2. Ensaladas")
+        print("3. Carnes")
+        print("4. Pizzas")
+        print("5. Postres")
+        print("6. Comida china")
+        print("~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+        especialidad=input()
+        if(especialidad=="1"):
+            q1 = "MATCH(especialidad {name: 'Comida Rapida'}) <-- (lugar) RETURN lugar.name"
+            nodes = session.run(q1)
+            print("Te recomendamos comer en: ")
+            for node in nodes:
+                print(node[0])
         if(especialidad=="2"):
             q1 = "MATCH(especialidad {name: 'Ensalada'}) <-- (lugar) RETURN lugar.name"
             nodes = session.run(q1)
             print("Te recomendamos comer en: ")
             for node in nodes:
                 print(node[0])
-                
         if(especialidad=="3"):
             q1 = "MATCH(especialidad {name: 'Carnes'}) <-- (lugar) RETURN lugar.name"
             nodes = session.run(q1)
@@ -120,3 +161,6 @@ while(opcion != 6):
         print("FIN PROGRAMA")
         
         
+
+
+
